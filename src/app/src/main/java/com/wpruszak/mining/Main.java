c8cabdddee7f8aa478037bc4c2c84244167a0a96
Wojciech Pruszak <info@wpruszak.com>
2017-11-12 21:02:00 +0100

    Initial commit.

---
+++ src/app/src/main/java/com/wpruszak/mining/Main.java
@@ -0,0 +1,8 @@
+package com.wpruszak.mining;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
---
+++ src/app/src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module com.wpruszak.mining {
+}
